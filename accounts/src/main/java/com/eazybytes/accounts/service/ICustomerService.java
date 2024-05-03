1db7b1d09eed05d3b66b35becd80d5a8e6cf3231
Cristopher Vergara <cristopher.vergara.colombo@outlook.com>
2024-05-02 21:17:21 -0400

    Se agrega API GatewayserverApplication

--- accounts/src/main/java/com/eazybytes/accounts/service/ICustomerService.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/ICustomerService.java
@@ -9,5 +9,5 @@ public interface ICustomerService {
      * @param mobileNumber -Inpyt Mobile Number
      * @return Customer Details based on a given mobileNumber
      */
-    CustomerDetailsDto fetchCustomerDetails(String mobileNumber);
+    CustomerDetailsDto fetchCustomerDetails(String mobileNumber,String correlationid);
 }
